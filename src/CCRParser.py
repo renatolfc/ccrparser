28abb4e304952e9c12dbc2f3f3f68b5f49c3d151
Renato Cunha <renatocunha@acm.org>
2012-08-03 23:03:08 -0300

    ccrparser: improved the parser so it can pass the new tests

--- src/CCRParser.py
+++ src/CCRParser.py
@@ -19,12 +19,13 @@ class CCRParser(object):
     poicolumns = (u'stretch', u'traffic', u'lane', u'reason', u'observation',
             u'start', u'end')
     poiregex = re.compile(r'^(?P<%s>.*)$\s*'
-            r'^.*:\s+(?P<%s>.*)$\s*'
-            r'^.*:\s+(?P<%s>.*)$\s*'
-            r'^.*:\s+(?P<%s>.*)$\s*'
-            r'^.*:\s+(?P<%s>.*)$\s*'
-            r'^.*:\s+(?P<%s>.*)$\s*'
-            r'^.*:\s+(?P<%s>.*)$' % poicolumns, re.MULTILINE|re.DOTALL)
+            r'^.*:\s?(?P<%s>.*)$\s*'
+            r'^.*:\s?(?P<%s>.*)$\s*'
+            r'^.*:\s?(?P<%s>.*)$\s*'
+            r'^.*:\s?(?P<%s>.*)$\s*'
+            r'^.*:\s?(?P<%s>.*)$\s*'
+            r'^.*:\s?(?P<%s>.*)$' % poicolumns, re.MULTILINE|re.DOTALL)
+    importantcolumns = (u'stretch', u'start', u'end', u'traffic')
 
     def match_to_dict(self, match):
         d = {}
@@ -34,7 +35,21 @@ class CCRParser(object):
                 d[column] = value if len(value) != 0 else u'-'
             except IndexError:
                 d[column] = u'-'
+
         return d
+
+    def _valid(self, match):
+        if match is None:
+            return False
+        for column in self.importantcolumns:
+            try:
+                value = match.group(column).strip()
+                if len(value) == 0:
+                    return False
+            except IndexError:
+                if column in self.importantcolumns:
+                    return False
+        return True
 
     def parse(self, page):
         alldata = []
@@ -45,5 +60,5 @@ class CCRParser(object):
             for p in soup.findAll('p'):
                 alldata.append(p.getText('\n'))
         matches = (self.poiregex.match(poi) for poi in alldata)
-        return (self.match_to_dict(match) for match in matches if match is not None)
+        return (self.match_to_dict(match) for match in matches if self._valid(match))
 
